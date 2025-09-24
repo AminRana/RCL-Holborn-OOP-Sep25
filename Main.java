5e43d518ecf9dde57dd0836812577f21d6944734
zacsta <71870402+zacsta@users.noreply.github.com>
2025-09-24 19:30:50 +0100

    Add files via upload
    Zac Makris - Hello World

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+  public static void main(String[] args) {
+    String fname = "Zac";
+    String lname = "Bumbaclout";
+    System.out.println("Hello " + fname + " " + lname);
+  }
+}
