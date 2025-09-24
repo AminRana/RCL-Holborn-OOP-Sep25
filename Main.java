4685571530fb9273a93527a65a27ca2b3a293411
Silviu Luncanu <luncanusilviu85@gmail.com>
2025-09-24 20:06:24 +0100

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 public class Main {
   public static void main(String[] args) {
-    String name = "Ibrahim Serunkuma";
-    System.out.println("Hello " + name);
+    System.out.println("My name");
+System.out.println("Silviu");
+System.out.println("Luncanu");
   }
 }
