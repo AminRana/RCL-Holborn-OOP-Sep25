1fabd7ebc0cf90e2ebae154485f50b278f799556
Silviu Luncanu <luncanusilviu85@gmail.com>
2025-10-02 22:10:16 +0100

    Add files via upload

---
+++ Datatypes.java
@@ -0,0 +1,26 @@
+package myPackage;
+public class Datatypes
+{
+public static void main(String[] args)
+{
+long x;
+int y;
+float temperature;
+double radius;
+char chi;
+boolean Pass;
+x = 2000 * 24 * 60 * 60;
+y = 12321;
+temperature =12/2;
+
+radius = 12.5;
+chi = 'Y';
+Pass = true;
+
+System.out.println("long integer: "+ x);
+System.out.println("integer: "+ y );
+System.out.println("float: "+ temperature );
+System.out.println("double value: "+ radius );
+System.out.println("Boolean: "+ Pass );
+}
+}
