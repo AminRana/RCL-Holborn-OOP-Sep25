c6922ca4e7da6cb6c09c6c320634a6b3ecad74d7
donabsworld <Donabsworld@outlook.com>
2025-10-09 19:11:58 +0100

    Add files via upload

---
+++ GradeCalculator.java
@@ -0,0 +1,32 @@
+package myPackage;
+
+public class GradeCalculator {
+    public static void main(String[] args) {
+        
+        int score = 90; 
+        char grade;
+
+        if (score >= 90 && score <= 100) {
+            grade = 'A';
+        } 
+        else if (score >= 80 && score <= 89) {
+            grade = 'B';
+        } 
+        else if (score >= 70 && score <= 79) {
+            grade = 'C';
+        } 
+        else if (score >= 60 && score <= 69) {
+            grade = 'D';
+        } 
+        else if (score < 60 && score >= 0) {
+            grade = 'F';
+        } 
+        else {
+            System.out.println("Invalid score! Please enter a value between 0 and 100.");
+            return;
+        }
+
+        System.out.println("Student Score: " + score);
+        System.out.println("Grade: " + grade);
+    }
+}
