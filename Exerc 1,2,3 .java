a92420cded0ba13ac99dd353f10a57bb1554a595
Silviu Luncanu <luncanusilviu85@gmail.com>
2025-10-15 19:45:36 +0100

    by Silviu Luncanu

---
+++ Exerc 1,2,3 .java
@@ -0,0 +1,54 @@
+/*package myPackage;
+
+public class wed15Oct {
+    public static void main(String[] args) {
+        int num1 = 223;
+        int num2 = 167;
+
+        if (num1 > num2) {
+            System.out.println("num1 is greater than num2");
+        }
+    }
+} 
+*/
+
+/*package myPackage;
+
+public class wed15Oct {
+    public static void main(String[] args) {
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
+        System.out.println(4);
+        System.out.println(5);
+        System.out.println(6);
+        System.out.println(7);
+        System.out.println(8);
+        System.out.println(9);
+        System.out.println(10);
+        System.out.println(99);
+    }
+}
+*/
+
+/*package myPackage;
+
+import java.util.Scanner;
+
+public class wed15Oct {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = input.nextInt();
+
+        int factorial = 1;
+
+        for (int i = 1; i <= num; i++) {
+            factorial = factorial * i;
+        }
+
+        System.out.println("Factorial of " + num + " is: " + factorial);
+        input.close();
+    }
+}
+*/
