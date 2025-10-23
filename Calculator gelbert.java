0d1a6a2f59aa9ceb3cb6d038bbec96875d71f95c
gelbertdomiciano-crypto <gelbertdomiciano@gmail.com>
2025-10-23 18:27:38 +0100

    Add files via upload

---
+++ Calculator gelbert.java
@@ -0,0 +1,31 @@
+public class Calculator {
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static double divide(int a, int b) {
+        if (b == 0) {
+            System.out.println("Error: Division by zero.");
+            return 0;
+        }
+        return (double) a / b;
+    }
+
+    public static void main(String[] args) {
+        int a = 12, b = 7;
+
+        System.out.println("Add: " + add(a, b));
+        System.out.println("Subtract: " + subtract(a, b));
+        System.out.println("Multiply: " + multiply(a, b));
+        System.out.println("Divide: " + divide(a, b));
+    }
+}
